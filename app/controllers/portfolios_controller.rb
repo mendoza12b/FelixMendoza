7946a91b9370f7ea4b1fbb68961f02e4e6e43855
felix mendoza <felixmendoza@felixs-MacBook-Pro.local>
2018-03-20 14:49:56 -0400

    Implemented image defaults for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -14,14 +14,16 @@ class PortfoliosController < ApplicationController
   def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
-
-      if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-      else
-        format.html { render :new }
+      respond_to do |format|
+        if @portfolio_item.save
+          format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+        else
+          format.html { render :new }
+        end
       end
-    
+     
   end
+
 
 def edit
   @portfolio_item = Portfolio.find(params[:id])
@@ -37,6 +39,7 @@ def update
       else
         format.html { render :edit }
         format.json { render json: @blog.errors, status: :unprocessable_entity }
+
       end
     end
   end
