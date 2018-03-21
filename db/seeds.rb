236823ba79e0344a90d5b7878b7d32c858404bb3
felix mendoza <felixmendoza@felixs-MacBook-Pro.local>
2018-03-20 21:28:23 -0400

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,3 +46,12 @@ thumb_image:"http://via.placeholder.com/350x200",
 end
 
 puts "9 portfolio items created"
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+    )
+
+end
+
+puts "3 technologies created"
