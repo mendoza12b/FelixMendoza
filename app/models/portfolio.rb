4cd15729b73bc785ef702e02f8a4fc7a8cb40536
Felix Mendoza <mendoza.felix@outlook.com>
2019-04-11 15:26:47 -0400

    Updated portfolio to includse attr for position

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -10,6 +10,10 @@ accepts_nested_attributes_for :technologies,
 		where(subtitle: 'Angular')
 	end
 
+	def self.by_position 
+		order("position ASC")
+	end 
+
 	scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
 
 	after_initialize :set_defaults 
